807387e84217456c09ea3c2b9147243faf3bd4f6
j-roylance <153141181+j-roylance@users.noreply.github.com>
2026-02-23 13:51:58 -0700

    random changes that were made before I typed a single line of code

--- Mission08_4-15/obj/Debug/net10.0/Mission08_4-15.AssemblyInfo.cs
+++ Mission08_4-15/obj/Debug/net10.0/Mission08_4-15.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission08_4-15")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1e9ab5f0387facb6793ff530248748cd944c62")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission08_4-15")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission08_4-15")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
