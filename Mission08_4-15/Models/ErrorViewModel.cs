7b1e9ab5f0387facb6793ff530248748cd944c62
jh022912 <thejakesterh@gmail.com>
2026-02-23 13:39:33 -0700

    Initial commit

---
+++ Mission08_4-15/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission08_4_15.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
